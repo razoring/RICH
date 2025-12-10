e7b919183afadb86502140cd0318f8d9d5f660a7
razoring <blotzytgaming@gmail.com>
2025-12-09 20:24:03 -0500

    embeds

--- index/index.py
+++ index/index.py
@@ -42,7 +42,12 @@ async def predict(interaction: discord.Interaction, ticker: str, model: typing.O
         if image_buffer:
             file = discord.File(image_buffer, filename="output.png")
             embed.set_image(url="attachment://output.png")
-            await interaction.followup.send(f"Here is today's predictions {interaction.user.mention}:",file=file, embed=embed)
+            embed.add_field(name="Open: $999.99", value="High: $999.99", inline=True)
+            embed.add_field(name="Low: $999.99", value="Mkt Cap: 99T", inline=True)
+            embed.add_field(name="Vol: 99M", value="Avg Vol: 99M", inline=True)
+            embed.add_field(name="52WK High: 99", value="52WK Low: 99", inline=True)
+            await interaction.followup.send(f"Here is today's 90 predictions {interaction.user.mention}:",file=file, embed=embed, title=f"{ticker} (90 day prediction)")
+
         else:
             await interaction.followup.send("```ERROR: Please check you entered the ticker symbol correct.```")
     except Exception as e:
